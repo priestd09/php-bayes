b56dced1589e2fa7a7b9fdcebea2d18680c8f908
Andrew Tch <andrew@noop.lv>
2011-12-26 21:00:50 +0200

    String tokenizer wip

---
+++ Tests/BayesTest.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once('../autoload.php');
+
+class BayesTest extends \PHPUnit_Framework_TestCase
+{
+    public function testStringTokenizer()
+    {
+        $tokenizer = new Noop\Bayes\Tokenizer\String();
+        
+        $this->assertEquals($tokenizer->tokenize('This is some string that should be tokenized tokenized'),
+                array('this' => 1, 'is' => 1, 'some' => 1, 'string' => 1, 'that' => 1, 'should' => 1, 'be' => 1, 'tokenized' => 2));
+        
+        $this->assertEquals($tokenizer->tokenize('А если utf-8?'), array('а' => 1, 'если' => 1, 'utf' => 1));
+    }
+}
