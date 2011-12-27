92db04138df12df186402eb9cfbf3f2470ec94ae
Andrew Tch <andrew@noop.lv>
2011-12-27 22:12:10 +0200

    Introduced encodings

--- Noop/Bayes/Tokenizer/AbstractTokenizer.php
+++ Noop/Bayes/Tokenizer/AbstractTokenizer.php
@@ -4,12 +4,32 @@ namespace Noop\Bayes\Tokenizer;
 
 abstract class AbstractTokenizer
 {
+    protected $encoding;
+    
+    protected $defaultEncoding = 'utf-8';
+    
     abstract public function tokenize($data);
+    
+    public function __construct()
+    {
+        $this->setEncoding($this->defaultEncoding);
+    }
     
     protected function normalizeValue($value)
     {
-        $value = mb_strtolower($value, 'utf-8');
+        $value = mb_strtolower($value);
         
         return $value;
+    }
+    
+    public function getEncoding()
+    {
+        return $this->encoding;
+    }
+
+    public function setEncoding($encoding)
+    {
+        $this->encoding = $encoding;
+        mb_internal_encoding($encoding);
     }
 }
--- Tests/BayesTest.php
+++ Tests/BayesTest.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('../autoload.php');
+require_once(__DIR__.'/../autoload.php');
 
 class BayesTest extends \PHPUnit_Framework_TestCase
 {
